4cf7120d9e6dfe480a0ebf09d344f38ab4cd35d1
chiragkhimani <khimanichirag@gmail.com>
2023-05-08 10:37:16 +0530

    data driven test

---
+++ src/test/java/com/automation/testcases/DataDrivenTest.java
@@ -0,0 +1,41 @@
+package com.automation.testcases;
+
+import com.ebay.LoginFeatures;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataDrivenTest {
+
+    LoginFeatures loginFeature = new LoginFeatures();
+
+    @Test(dataProvider = "invalidData")
+    public void verifyLoginUnsuccessfulWithInvalidCredentials(String username, String password) {
+        // Testing the Method with Test Data
+        boolean result = loginFeature.doLogin(username, password);
+
+        // Checking the output
+        Assert.assertEquals(result, false);
+    }
+
+    @DataProvider(name = "invalidData")
+    public Object[][] getInvalidData() {
+        String[][] credentials = {
+                {"admin", "admin@123"},
+                {"admin", "admin123"},
+                {"admin123", "admin123"},
+                {"chirag", "admin123"},
+                {"admin", "@123"},
+                {"devx", "devx@123"},
+                {"", ""},
+                {"", "admin123"}
+        };
+        return credentials;
+    }
+
+
+    @Test
+    public void verifyLoginSuccessfulWithValidCred() {
+
+    }
+}
--- src/test/java/com/automation/testcases/LoginTest.java
+++ src/test/java/com/automation/testcases/LoginTest.java
@@ -1,16 +1,19 @@
 package com.automation.testcases;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
 
     @Test(groups = {"SmokeTest", "ChiragTest"})
-    public void verifyUserCanLogin() {
+    @Parameters("browser")
+    public void verifyUserCanLogin(String browser) {
         System.out.println("verify login successful");
+        System.out.println("Opening browser  " + browser);
     }
 
-    @Test(groups = {"ChiragTest" ,"WIPTest"})
+    @Test(groups = {"ChiragTest", "WIPTest"})
     public void verifyUserCannotLoginWithInvalidCredentials() {
         System.out.println("verify login unsuccessful for invalid credentials");
     }
